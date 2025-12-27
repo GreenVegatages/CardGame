8e959180f027bbd239efaec639a462ea3542aab5
xuchen <xuchen1605@gmail.com>
2025-12-27 19:42:41 +0800

    创建逻辑帧同步配置文件

---
+++ Assets/Scripts/Logic/Config/LogicFrameSyncConfig.cs
@@ -0,0 +1,20 @@
+﻿public class LogicFrameSyncConfig
+{
+        
+        /// <summary>
+        /// 逻辑帧Id
+        /// </summary>
+        public static long LogicFrameId;
+        
+        /// <summary>
+        /// 逻辑帧间隔 1s 15帧 1/60 = 0.066f
+        /// </summary>
+        public static float LogicFrameInterval = 0.066f; //
+        
+        /// <summary>
+        /// 逻辑帧间隔Ms 
+        /// </summary>
+        public static float LogicFrameIntervalMs = 66f;
+
+
+}
