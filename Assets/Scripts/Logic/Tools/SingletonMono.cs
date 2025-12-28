a638d1387bbc1ccdbaad5950d5fc79dca8c5f818
xuchen <xuchen1605@gmail.com>
2025-12-28 14:38:02 +0800

    单例mono类

---
+++ Assets/Scripts/Logic/Tools/SingletonMono.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SingletonMono<T> : MonoBehaviour where T : SingletonMono<T>
+{
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                    if (_instance == null)
+                    {
+                        GameObject obj = new GameObject(typeof(T).Name);
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+                return _instance;
+            }
+        }
+}
