deadc534d6740ac588a7256b2b45bce0ab1a5895
xuchen <xuchen1605@gmail.com>
2026-01-11 20:40:28 +0800

    导入定点数库

--- Assets/Scripts/Logic/Interface/LogicBehaviour.cs
+++ Assets/Scripts/Logic/Interface/LogicBehaviour.cs
@@ -1,4 +1,9 @@
-﻿public class LogicBehaviour
+﻿/// <summary>
+/// 负责基础的属性和接口，不负责实现
+/// </summary>
+public class LogicBehaviour 
 {
-        
+    public RenderObject RenderObj { get;protected set; }
+    
+    
 }
